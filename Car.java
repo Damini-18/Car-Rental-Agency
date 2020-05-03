53f01a6ada40cc6c66848a3ecfda11f965b3d922
Damini-18 <64721859+Damini-18@users.noreply.github.com>
2020-05-03 11:51:18 +0530

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,15 @@
+package CarRentalAgency;
+
+import java.util.Date;
+
+public class Car {
+
+	public String Vehicle_no="";
+	public String Model="";
+	public int Seating_capacity;
+	public int rent_per_day;
+	public String status;
+	public Date date;
+	
+	
+}
